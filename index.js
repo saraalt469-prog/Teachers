8274775e6a3db17010ce684cf4a4b97000c90f76
Elena Domanskaia <elena@MacBook-Air-Elena.local>
2026-02-21 23:11:20 +0300

    Update index.html and index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const form = document.querySelector(".form");
 
-let data = new Date();
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const name = document.querySelector(".name").value.trim();
@@ -12,6 +10,7 @@ form.addEventListener("submit", (e) => {
     alert("Заполните все поля");
     return;
   }
+  let data = new Date();
 
   const tb = document.querySelector(".table");
 
@@ -26,9 +25,14 @@ form.addEventListener("submit", (e) => {
   const phoneC = document.createElement("td");
   phoneC.textContent = phone;
 
+  const d = document.createElement("td");
+
+  d.textContent = `Дата создания:  ${data.getDate()}.${data.getMonth() + 1}.${data.getFullYear()} `;
+
   row.appendChild(nc);
   row.appendChild(lastC);
   row.appendChild(phoneC);
+  row.appendChild(d);
 
   tb.appendChild(row);
 
@@ -48,6 +52,7 @@ form2.addEventListener("submit", (e) => {
     alert("Заполните все поля");
     return;
   }
+  let date = new Date();
   let wr = document.querySelector(".exercise-wr");
 
   let block = document.createElement("div");
@@ -60,9 +65,12 @@ form2.addEventListener("submit", (e) => {
   let text = document.createElement("p");
   text.textContent = `Описание: ${exercise2}`;
   text.classList.add("exercise-text");
+  let dateS = document.createElement("p");
+  dateS.textContent = `Дата создания: ${date.getDate()} . ${date.getMonth() + 1} . ${date.getFullYear()}`;
 
   block.appendChild(title);
   block.appendChild(text);
+  block.appendChild(dateS);
   wr.appendChild(block);
 
   document.querySelector(".exercise").value = "";
